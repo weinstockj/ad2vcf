0c2a9d87b4d6206ca4404330d2d2c676ae29df00
Jason Bacon <bacon@barracuda.uits.uwm.edu>
2019-12-08 10:56:20 -0600

    Add arg processing and processing function

--- ad2vcf.c
+++ ad2vcf.c
@@ -15,15 +15,17 @@
 
 #include <stdio.h>
 #include <sysexits.h>
+#include <string.h>
 #include <stdlib.h>
 #include "ad2vcf.h"
 
 int     main(int argc, const char *argv[])
 
 {
-    if ( argc != 1 )
+    if ( argc != 2 )
 	usage(argv);
-    return EX_OK;
+    
+    return ad2vcf(argv);
 }
 
 
@@ -33,4 +35,32 @@ void    usage(const char *argv[])
     fprintf(stderr, "Usage: %s single-sample-VCF-file\n", argv[0]);
     fputs("SAM stream expected on stdin.\n", stderr);
     exit(EX_USAGE);
+}
+
+
+/***************************************************************************
+ *  Description:
+ *      1. Get list of positions from VCF file
+ *      2. Get allele counts for each position from SAM stream
+ *
+ *  History: 
+ *  Date        Name        Modification
+ *  2019-12-08  Jason Bacon Begin
+ ***************************************************************************/
+
+int     ad2vcf(const char *argv[])
+
+{
+    FILE        *vcf_stream;
+    extern int  errno;
+    
+    if ( (vcf_stream = fopen(argv[1], "r")) == NULL )
+    {
+	fprintf(stderr, "%s: Cannot open %s: %s\n", argv[0], argv[1],
+	    strerror(errno));
+	exit(EX_NOINPUT);
+    }
+    
+    fclose(vcf_stream);
+    return EX_OK;
 }
