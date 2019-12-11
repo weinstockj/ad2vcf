e8e353b7cb9070be5c0e2171599c9936e132b93f
Jason Bacon <bacon@barracuda.uits.uwm.edu>
2019-12-11 12:44:20 -0600

    Handle duplicate VCF calls and save allele output for each POS

--- ad2vcf.c
+++ ad2vcf.c
@@ -54,18 +54,31 @@ void    usage(const char *argv[])
 int     ad2vcf(const char *argv[], FILE *sam_stream)
 
 {
-    FILE            *vcf_stream;
-    vcf_call_t      vcf_call;
+    FILE            *vcf_stream,
+		    *allele_stream;
+    vcf_duplicate_call_t    vcf_duplicate_calls;
     sam_alignment_t sam_alignment;
     extern int      errno;
-    int             more_alignments;
-    char            genotype[VCF_GENOTYPE_NAME_MAX + 1];
+    int             more_alignments,
+		    allele;
+    size_t          vcf_call_pos;
+    char            genotype[VCF_GENOTYPE_NAME_MAX + 1],
+		    *allele_file = "alleles.txt",
+		    *vcf_call_chromosome;
     
     if ( (vcf_stream = fopen(argv[1], "r")) == NULL )
     {
 	fprintf(stderr, "%s: Cannot open %s: %s\n", argv[0], argv[1],
 	    strerror(errno));
 	exit(EX_NOINPUT);
+    }
+    
+    
+    if ( (allele_stream = fopen(allele_file, "w")) == NULL )
+    {
+	fprintf(stderr, "%s: Cannot open %s: %s\n", argv[0], allele_file,
+	    strerror(errno));
+	exit(EX_CANTCREAT);
     }
     
     /*
@@ -84,11 +97,20 @@ int     ad2vcf(const char *argv[], FILE *sam_stream)
     // Maybe create vcf_read_duplicate_calls() to read in all consecutive
     // calls with the same position.  Then we can check each SAM alignment
     // against all ALT alleles.
-    while ( more_alignments && vcf_read_call(argv, vcf_stream, &vcf_call) )
+    while ( more_alignments && vcf_read_duplicate_calls(argv, vcf_stream,
+				&vcf_duplicate_calls) )
     {
+	// All the same
+	vcf_call_pos = vcf_duplicate_calls.vcf_call[0].pos;
+	vcf_call_chromosome = vcf_duplicate_calls.vcf_call[0].chromosome;
+	// fprintf(stderr, "VCF call pos = %zu\n", vcf_call_pos);
+	if ( vcf_duplicate_calls.count > 1 )
+	    fprintf(stderr, "Read %zu duplicate calls at pos %zu.\n",
+		    vcf_duplicate_calls.count, vcf_call_pos);
+	
 	// vcf_read_call() only gets static fields, not sample data
 	// Single-sample inputs should just have one genotype in addition
-	tsv_read_field(argv, vcf_stream, genotype, VCF_GENOTYPE_NAME_MAX);
+	fprintf(allele_stream, "%zu ", vcf_call_pos);
 	
 	/*
 	 *  Now check all SAM alignments for the same chromosome with sequence
@@ -100,8 +122,8 @@ int     ad2vcf(const char *argv[], FILE *sam_stream)
 	 *  Do integer position compare before strcmp().  It's less intuitive
 	 *  to check the second sort key first, but faster.
 	 */
-	while ( more_alignments && (sam_alignment.pos <= vcf_call.pos) &&
-	       (strcmp(vcf_call.chromosome, sam_alignment.rname) == 0) )
+	while ( more_alignments && (sam_alignment.pos <= vcf_call_pos) &&
+	       (strcmp(vcf_call_chromosome, sam_alignment.rname) == 0) )
 	{
 	    // FIXME: Check all VCF records for the same position.
 	    /*
@@ -110,17 +132,22 @@ int     ad2vcf(const char *argv[], FILE *sam_stream)
 	     *  upstream of the end?  If so, record the exact position and
 	     *  allele.
 	     */
-	    if ( vcf_call.pos <= sam_alignment.pos + sam_alignment.seq_len )
+	    if ( vcf_call_pos <= sam_alignment.pos + sam_alignment.seq_len )
 	    {
+		allele = sam_alignment.seq[vcf_call_pos - sam_alignment.pos];
+#ifdef DEBUG
 		fprintf(stderr, "===\n%s pos=%zu len=%zu %s\n",
 			sam_alignment.rname, sam_alignment.pos,
 			sam_alignment.seq_len, sam_alignment.seq);
 		fprintf(stderr, "Found allele %c for call pos %zu on %s aligned seq starting at %zu.\n",
-			sam_alignment.seq[vcf_call.pos - sam_alignment.pos],
-			vcf_call.pos, vcf_call.chromosome, sam_alignment.pos);
+			allele, vcf_call.pos, vcf_call_chromosome,
+			sam_alignment.pos);
+#endif
+		putc(allele, allele_stream);
 	    }
 	    more_alignments = sam_read_alignment(argv, sam_stream, &sam_alignment);
 	}
+	putc('\n', allele_stream);
     }
     
     fclose(vcf_stream);
