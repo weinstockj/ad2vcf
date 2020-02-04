5eb1c80ae76ef713036e54b52dbab6c0e1dae673
Jason Bacon <bacon@barracuda.uits.uwm.edu>
2020-02-04 13:58:42 -0600

    Remove restrict modifier from chromosome_name_cmp()

--- ad2vcf-protos.h
+++ ad2vcf-protos.h
@@ -3,4 +3,4 @@ int main(int argc, const char *argv[]);
 void usage(const char *argv[]);
 int ad2vcf(const char *argv[], FILE *sam_stream);
 int sam_read_alignment(const char *argv[], FILE *sam_stream, sam_alignment_t *sam_alignment);
-int chromosome_name_cmp(const char *restrict n1, const char *restrict n2);
+int chromosome_name_cmp(const char *n1, const char *n2);
